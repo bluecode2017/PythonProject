9ceba3b2981d7dd013f7f8de1e510278368446b9
Bluecode2017 <blackhair2016@gmail.com>
2016-12-17 22:37:13 -0800

    Update Top100movie_crawler.py

--- Top100movie_crawler.py
+++ Top100movie_crawler.py
@@ -5,7 +5,7 @@ import string
 import re
 import urllib2
 
-#create a simple spider and use it to crawl Top 100 moive's name 
+#create a simple spider and use it to crawl Top 100 moive's name
 #from https://movie.douban.com/top250?start={page}&filter=&type=
 #and output the result list
 
@@ -13,7 +13,7 @@ class DouBanSpider(object):
     def __init__(self):
         self.page=1
         self.current_url="http://movie.douban.com/top250?start={page}&filter=&type="
-        self.datas=[]
+        self.data=[]
         self._top_num=1
         print " begin to scrap the top 100 movies from Douban website..."
     def get_page(self,cur_page):
@@ -30,22 +30,25 @@ class DouBanSpider(object):
         return my_page
     def find_title(self,my_page):
         temp_data=[]
+        # in source html: <span class="title">肖申克的救赎</span>
         movie_items=re.findall(r'<span.*?class="title">(.*?)</span>',my_page,re.S)
+
+    #    director_items=re.findall(r'<span.*?class="title">(.*?)</span>',my_page,re.S)
         for index,item in enumerate(movie_items):
             if item.find("&nbsp")==-1:
                 temp_data.append("Top"+str(self._top_num)+" "+item)
                 self._top_num+=1
-        self.datas.extend(temp_data)
+        self.data.extend(temp_data)
     def start_spider(self):
         while self.page<=4:
             my_page=self.get_page(self.page)
             self.find_title(my_page)
             self.page+=1
 def main():
-    print "scrap Top 100 movie from movie.douban.com"
+    print "scrap Top 100 movie from moviw.douban.com"
     my_spider=DouBanSpider()
     my_spider.start_spider()
-    for item in my_spider.datas:
+    for item in my_spider.data:
         print item
     print "finish scrap data"
 
