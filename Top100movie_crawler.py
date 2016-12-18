e4c71932ec2086e7b70d7d294a1b9f6c32f6c041
Bluecode2017 <blackhair2016@gmail.com>
2016-12-17 22:27:30 -0800

    Create Top100movie_crawler.py

---
+++ Top100movie_crawler.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env Python
+# _*_ coding:utf-8 _*_
+
+import string
+import re
+import urllib2
+
+#create a simple spider and use it to crawl Top 100 moive's name 
+#from https://movie.douban.com/top250?start={page}&filter=&type=
+#and output the result list
+
+class DouBanSpider(object):
+    def __init__(self):
+        self.page=1
+        self.current_url="http://movie.douban.com/top250?start={page}&filter=&type="
+        self.datas=[]
+        self._top_num=1
+        print " begin to scrap the top 100 movies from Douban website..."
+    def get_page(self,cur_page):
+        url=self.current_url
+        try:
+            my_page=urllib2.urlopen(url.format(page=(cur_page-1)*25)).read().decode("utf-8")
+        except urllib2.URLERROR, e:
+            if hasattr(e,"code"):
+                print "The server could not fulfill the request."
+                print "Error code is %s" %e.code
+            elif hasattr(e,"reason"):
+                print "We failed to reach a server。 please check your url and read the reason."
+                print "Reson %s" %e.reason
+        return my_page
+    def find_title(self,my_page):
+        temp_data=[]
+        movie_items=re.findall(r'<span.*?class="title">(.*?)</span>',my_page,re.S)
+        for index,item in enumerate(movie_items):
+            if item.find("&nbsp")==-1:
+                temp_data.append("Top"+str(self._top_num)+" "+item)
+                self._top_num+=1
+        self.datas.extend(temp_data)
+    def start_spider(self):
+        while self.page<=4:
+            my_page=self.get_page(self.page)
+            self.find_title(my_page)
+            self.page+=1
+def main():
+    print "scrap Top 100 movie from movie.douban.com"
+    my_spider=DouBanSpider()
+    my_spider.start_spider()
+    for item in my_spider.datas:
+        print item
+    print "finish scrap data"
+
+if __name__=='__main__':
+    main()
